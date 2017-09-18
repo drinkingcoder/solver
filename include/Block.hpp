2a24c8f7ce27e9395f1080feebb2423af4195ecc
drinkingcoder <drinkingcoder@gmail.com>
2017-09-18 13:17:46 +0800

    Add some code

--- include/Block.hpp
+++ include/Block.hpp
@@ -13,6 +13,30 @@ using namespace std;
 
 namespace DCSolver
 {
+	template<typename DataType>
+	struct Variable
+	{
+		std::shared_ptr<DataType> x;		//Ax = b,	x: Nx1
+		int N;								//length of x
+		Variable(std::shared_ptr<DataType> x,int N):x(x),N(N) {}
+	};
+
+	template<typename DataType>
+	struct Block
+	{
+		std::shared_ptr<DataType> A;		//Ax = b,	A: x1->N x x2->N
+		std::shared_ptr<Variable> x1,x2;	//x1 - row, x2 - col
+		Block(std::shared_ptr<DataType> A,std::shared_ptr<Variable> x1,std::shared_ptr<Variable> x2):A(A),x1(x1),x2(x2) {}
+	};
+
+	template<typename DataType>
+	struct Param
+	{
+		std::shared_ptr<DataType> b;		//Ax = b,	b: x->Nx1
+		std::shared_ptr<Variable> x;		//corresponding x
+		Param(std::shared_ptr<DataType> b,std::shared_ptr<Variable> x):b(b),x(x) {}
+	};
+
 //    struct Position
 //    {
 //        int r,c;
--- include/LinearProblem.hpp
+++ include/LinearProblem.hpp
@@ -7,6 +7,7 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<map>
 #include"Block.hpp"
 
 using namespace std;
@@ -29,20 +30,40 @@ namespace DCSolver
 //    };
     class LinearProblem
     {
+		std::vector<std::shared_ptr<Variable>> 	m_xs;
+		std::vector<std::shared_ptr<Block>>		m_As;
+		std::vector<std::shared_ptr<Param>>		m_bs;
+
+		std::map<std::shared_ptr<DataType>,std::shared_ptr<Variable>> m_mapRawToVariable;	//map the input raw data ptr to variable ptr
     public:
+		enum Status {
+			ADD_FAILED,
+			ADD_SUCCESS,
+			TOTAL_STATUS
+		};
+
         void addx(std::shared_ptr<DataType> x, int N)
         {
-
+			m_xs.push_back(std::make_shared<Variable>(x,N));
         }
 
         int setA(std::shared_ptr<DataType> x1,std::shared_ptr<DataType> x2,std::shared_ptr<DataType> A)
         {
+			auto it = m_xs.find(m_xs.begin(),m_xs.end(),x1);
+			assert( *it != m_xs.end() );
+			std::shared_ptr<Variable> tmp_x1 = *it;
 
+			it = m_xs.find(m_xs.begin(), m_xs.end(), x2);
+			assert( *it != m_xs.end() );
+			std::shared_ptr<Variable> tmp_x2 = *it;
+
+			m_As.push_back(std::make_shared<Block>(A, tmp_x1, tmp_x2);
+			return ADD_SUCCESS;
         }
 
         int setb(std::shared_ptr<DataType> x,std::shared_ptr<DataType> b)
         {
-
+			
         }
 
 
